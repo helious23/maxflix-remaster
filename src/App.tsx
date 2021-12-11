af37bd04a9798cd35dd788ebf576fafaa795df4a
Max16 <max16@naver.com>
2021-12-11 11:16:34 +0900

    recoil selector set

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { GlobalStyle } from "./GlobalStyle";
-import { ToDoList } from "./components/ToDoList";
+import { useRecoilState } from "recoil";
+import { minuteState, hourSelector } from "./atoms";
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
   return (
-    <>
-      <GlobalStyle />
-      <ToDoList />
-    </>
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import { RecoilRoot } from "recoil";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { darkTheme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
       <ThemeProvider theme={darkTheme}>
+        <GlobalStyle />
         <App />
       </ThemeProvider>
     </RecoilRoot>
