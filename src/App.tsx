32bb5d704f29126aa0e2e33455d7fcb67eccbe59
Max16 <max16@naver.com>
2021-12-14 10:59:18 +0900

    #7.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 library.add(fas);
 
@@ -23,34 +24,35 @@ const Box = styled(motion.div)`
   grid-template-columns: repeat(2, 1fr);
 `;
 
-const Circle = styled(motion.div)`
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  background-color: #fff;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 `;
 
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px", rotateZ: 0 },
-  drag: {
-    backgroundColor: "rgba(0, 206, 201,1.0)",
-    transition: { duration: 3 },
-  },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileDrag="drag"
-        whileHover="hover"
-        whileTap="click"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
