bf64a8d02c315e2345185de8d1ead1439e9beb3a
Max16 <max16@naver.com>
2021-12-14 09:49:30 +0900

    #7.5 Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: grid;
@@ -33,43 +33,24 @@ const Circle = styled(motion.div)`
 `;
 
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.3,
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px", rotateZ: 0 },
+  drag: {
+    backgroundColor: "rgba(0, 206, 201,1.0)",
+    transition: { duration: 3 },
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box
+        drag
+        variants={boxVariants}
+        whileDrag="drag"
+        whileHover="hover"
+        whileTap="click"
+      />
     </Wrapper>
   );
 }
