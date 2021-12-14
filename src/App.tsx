f04b5c0f4067a93df126898d3cdf0b9272a94dfe
Max16 <max16@naver.com>
2021-12-14 18:21:02 +0900

    #7.13 Slider

--- src/App.tsx
+++ src/App.tsx
@@ -15,70 +15,73 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg, rgb(0, 56, 238), rgb(0, 151, 238));
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 400px;
+  height: 300px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
   position: absolute;
   top: 100px;
-  grid-template-columns: repeat(2, 1fr);
 `;
 
-const boxVariants = {
-  initial: {
+const box = {
+  entry: (back: boolean) => ({
+    x: back ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
     transition: { duration: 0.5 },
   },
-  leaving: {
+  exit: (back: boolean) => ({
+    x: back ? 500 : -500,
     opacity: 0,
-    y: -20,
+    scale: 0,
     transition: { duration: 0.5 },
-  },
+  }),
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(0, 56, 238), rgb(0, 151, 238))",
-      "linear-gradient(135deg, rgb(0, 238, 91), rgb(172, 231, 209))",
-    ]
-  );
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
-    <Wrapper style={{ background: gradient }}>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-            drag="x"
-            dragSnapToOrigin
-            style={{ x, rotateZ }}
-          />
-        ) : null}
+    <Wrapper>
+      <AnimatePresence custom={back}>
+        <Box
+          custom={back}
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
-      <button onClick={toggleShowing}>Click</button>
+      <button onClick={prevPlease}>Prev</button>
+      <button onClick={nextPlease}>Next</button>
     </Wrapper>
   );
 }
